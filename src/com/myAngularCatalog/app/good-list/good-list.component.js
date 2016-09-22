9f2d1eae46a8c3925a528559f91e1a928fa391e0
lena <lena-gobova@yandex.u>
2016-09-22 15:38:19 +0300

    Initial commit

---
+++ src/com/myAngularCatalog/app/good-list/good-list.component.js
@@ -0,0 +1,51 @@
+'use strict';
+
+// Register `phoneList` component, along with its associated controller and template
+angular.module('goodList').component('goodList', {
+    templateUrl: 'good-list/good-list.template.html',
+    controller: ['$http', function GoodListController($http) {
+      // this.goods = [
+      //   {
+      //     name: 'Кефир',
+      //     snippet: 'Способствует восстановлению баланса бифидобактерий в организме.',
+      //     price: 20
+      //   }, {
+      //     name: 'Творог',
+      //     snippet: 'Способствует укреплению и здоровому росту мышц, костей и других тканей организма.',
+      //     price: 40
+      //   }, {
+      //     name: 'Ряженка',
+      //     snippet: 'Способствует очищению организма.',
+      //     price: 50
+      //   }
+      // ];
+
+      var self = this;
+      self.orderProp = 'price';
+
+      $http.get('goods/goods.json').then(function (response) {
+        self.goods = response.data;
+        self.phones = response.data.slice(0, 5);
+      });
+    }]
+  });
+
+/*
+ angular.
+ module('phoneList').
+ component('phoneList', {
+ templateUrl: 'phone-list/phone-list.template.html',
+ controller: function PhoneListController($http) {
+ var self = this;
+ self.orderProp = 'age';
+
+
+ $http.get('phones/phones.json').then(function(response) {
+ self.phones = response.data;
+ self.phones = response.data.slice(0, 5);
+ });
+
+
+ }
+ });
+ */
